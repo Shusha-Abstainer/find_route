dc8cbce7f4d4cabb3ee34341c2e31bc17d9135e2
Sanya <a.logvinov.com@gmail.com>
2022-11-22 22:05:21 +0300

    Adding page 'create.html'on base of home.html and adding button 'Save', saving the city
    Adding file forms.py with ClassBasedForms in Django
    
    and with this adding in files urls.py -
                                   additional url
    in views.py - class CreateView
    in cities/home.html submit button with 'post' method with csrf_token

--- cities/views.py
+++ cities/views.py
@@ -1,24 +1,38 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView
 
+from cities.forms import HtmlForm, CityForm
 from cities.models import City
 
 __all__ = (
-    'home', 'CityDetailView'
+    'home', 'CityDetailView', 'CityCreateView'
 )
 
 
 def home(request, pk=None):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
     # if pk:
     #     city = get_object_or_404(City, id=pk)
     #     context = {'object': city}
     #     return render(request, 'cities/detail.html', context)
-
+    form = CityForm()
     qs = City.objects.all()
-    context = {'objects_list': qs}
+    context = {'objects_list': qs, 'form': form}
     return render(request, 'cities/home.html', context)
 
 
 class CityDetailView(DetailView):
     queryset = City.objects.all()
     template_name = 'cities/detail.html'
+
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('cities:home')
